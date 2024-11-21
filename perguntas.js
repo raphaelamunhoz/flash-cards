11f7001c3cde02c0119676adb6f154fb0ffb979d
raphaelamunhoz <00001103189566sp@al.educacao.sp.gov.br>
2024-11-21 11:48:20 -0300

    adicionei perguntas

--- perguntas.js
+++ perguntas.js
@@ -1,13 +1,13 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'veterinaria',
+    'Porque o gato tem medo de pepino?',
+    'Esta questão e as suas variações resultam da partilha de vídeos virais em que gatos se assustam com pepinos, dando saltos engraçados. Mas então porque é que os gatos têm medo de pepinos? Roger Mugdrod, um especialista em comportamento animal, defende que o susto resulta da surpresa. Não é natural termos pepinos no chão ou pela casa. Assim, quando o gato vê de repente um pepino num local que não está à espera assusta-se. Sem tempo de processar a informação, o pepino é percecionado como potencialmente perigoso e o gato assusta-se, dando um salto. Esta resposta permite proteger os felinos na natureza quando se encontram repentinamente com perigos, como por exemplo com cobras.'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'veterinaria selvagem',
+    'Qual é o predador natural do javali?',
+    'No Brasil, o javali não encontra predador natural. Nem mesmo a onça-pintada ousa enfrentar esse bicho, dado o tamanho e a agressividade dos javalis adultos. A exceção pode se dar para animais muito jovens ou doentes. A ausência de predadores naturais, combinada com sua alta taxa de reprodução e adaptabilidade, torna os javalis uma espécie invasora altamente problemática. Isso explica a crescente preocupação com a manutenção da caça, como forma de mitigar o impacto ambiental e econômico causado por esses animais.'
 )
 
 criaCartao(
